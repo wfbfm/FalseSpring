1ff18a9149c4ad7c54e7bf584847016238f1b1e4
Billy Micou <wfbfmicou@gmail.com>
2023-04-11 22:54:39 +0200

    Added footer

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,6 +3,7 @@ import * as ReactDOMClient from "react-dom/client";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import Nav from "./navbar";
 import Grid from "./grid";
+import SmallWithLogoLeft from "./footer";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
   </div>
   <div>
   <Grid />
+  </div>
+  <div>
+  <SmallWithLogoLeft />
   </div>
   </>
   );
