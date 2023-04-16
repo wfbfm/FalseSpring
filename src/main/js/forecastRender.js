561a51b91f9a31370c9f71e0ea47c9153619e9f2
wfbfm <wfbfmicou@gmail.com>
2023-04-16 19:02:16 +0100

    Added mapping for all weather

--- src/main/js/forecastRender.js
+++ src/main/js/forecastRender.js
@@ -4,8 +4,9 @@ import { testLocationForecasts } from './data';
 import { TbClockHour10 } from 'react-icons/tb';
 import { IoWaterSharp } from 'react-icons/io5';
 import { GiWindsock, GiBinoculars } from 'react-icons/gi';
-import { BsThermometerHalf, BsCloudFill, BsFillCloudSunFill, BsSun } from 'react-icons/bs';
+import { BsThermometerHalf, BsCloudFill, BsFillCloudSunFill, BsSun, BsCloudSun, BsCloudyFill, BsCloudy, BsCloudDrizzle, BsCloudFog, BsCloudHazeFill, BsCloudRain, BsCloudRainHeavyFill, BsCloudSleet, BsCloudSleetFill, BsCloudHail, BsCloudHailFill, BsCloudSnow, BsCloudSnowFill, BsCloudLightningRainFill, BsCloudMoon, BsMoon } from 'react-icons/bs';
 import { HiArrowNarrowUp } from 'react-icons/hi';
+import { WiShowers } from 'react-icons/wi';
 
 
 function convertForecastData(locationForecasts) {
@@ -58,12 +59,73 @@ function mapWeatherIcon(hourlyForecast) {
     case 'Sunny':
       icon = BsSun;
       break;
+    case 'Sunny Intervals':
+      icon = BsCloudSun;
+      break;
+    case 'Light Cloud':
+      icon = BsCloudy;
+      break;
+    case 'Thick Cloud':
+      icon = BsCloudyFill;
+      break;
+    case 'Drizzle':
+      icon = BsCloudDrizzle;
+      break;
+    case 'Mist':
+      icon = BsCloudFog;
+      break;
+    case 'Fog':
+      icon = BsCloudHazeFill;
+      break;
+    case 'Light Rain Showers':
+      icon = BsCloudDrizzle;
+      break;
+    case 'Light Rain':
+      icon = BsCloudRain;
+      break;
+    case 'Heavy Rain':
+      icon = BsCloudRainHeavyFill;
+      break;
+    case 'Sleet Showers':
+      icon = BsCloudSleet;
+      break;
+    case 'Sleet':
+      icon = BsCloudSleetFill;
+      break;
+    case 'Hail Showers':
+      icon = BsCloudHail;
+      break;
+    case 'Hail':
+      icon = BsCloudHailFill;
+      break;
+    case 'Light Snow Showers':
+      icon = BsCloudSnow;
+      break;
+    case 'Light Snow':
+      icon = BsCloudSnow;
+      break;
+    case 'Heavy Snow':
+      icon = BsCloudSnowFill;
+      break;
+    case 'Thundery Showers':
+      icon = BsCloudLightningRainFill;
+      break;
+    case 'Partly Cloudy':
+      icon = BsCloudMoon;
+      break;
+    case 'Clear Sky':
+      icon = BsMoon;
+      break;
     default:
       icon = BsCloudFill;
   }
   icon.key = `weatherIcon-${hourlyForecast.forecast.id}`
   return icon;
 }
+
+const RotatedArrowIcon = React.forwardRef(({ rotation, ...rest }, ref) => (
+  <Icon as={HiArrowNarrowUp} ref={ref} style={{ transform: `rotate(${rotation}deg)` }} {...rest} />
+));
 
 function mapWindDirection(hourlyForecast) {
   switch (hourlyForecast.forecast.windDirectionAbbreviation) {
@@ -105,30 +167,28 @@ function mapWindDirection(hourlyForecast) {
 }
 
 function renderHourlyForecast(hourlyForecast) {
-  const windRotation = 'rotate(' + mapWindDirection(hourlyForecast) + 'deg)'
-  console.log(windRotation);
   return (
-    <>
+    <Box key={`outer-box-${hourlyForecast.forecast.id}`}>
       <Stack key={hourlyForecast.forecast.id} direction='column' align='center' justify='center' alignContent='center' alignItems='center'>
-        <Text as='i' fontSize='xs'>{hourlyForecast.timeslot}</Text>
+        <Text key={`timeslot-text-${hourlyForecast.forecast.id}`} as='i' fontSize='xs'>{hourlyForecast.timeslot}</Text>
         <Box>
           <Tooltip label={hourlyForecast.forecast.weatherTypeText} bg='red.600'>
             <Icon as={mapWeatherIcon(hourlyForecast)}></Icon>
           </Tooltip>
         </Box>
-        <Text display='flex' alignItems='center' as='b'>{hourlyForecast.forecast.temperatureC}°</Text>
-        <Box>{hourlyForecast.forecast.precipitationProbabilityInPercent}%</Box>
-        <Box display='flex' alignContent='center' justifyContent='center'>
-          <HStack spacing={0} justifyContent='center'>
-            <Tooltip label={hourlyForecast.forecast.windDirectionAbbreviation} bg='red.600'>
-              <Icon as={HiArrowNarrowUp} style={{ transform: {windRotation}}}></Icon>
+        <Text key={`temp-text-${hourlyForecast.forecast.id}`} display='flex' alignItems='center' as='b'>{hourlyForecast.forecast.temperatureC}°</Text>
+        <Box key={`precipitation-text-${hourlyForecast.forecast.id}`}>{hourlyForecast.forecast.precipitationProbabilityInPercent}%</Box>
+        <Box key={`wind-box-${hourlyForecast.forecast.id}`} display='flex' alignContent='center' justifyContent='center'>
+          <HStack key={`wind-stack-${hourlyForecast.forecast.id}`} spacing={0} justifyContent='center'>
+            <Tooltip key={`wind-tooltip-${hourlyForecast.forecast.id}`} label={hourlyForecast.forecast.windDirectionAbbreviation} bg='red.600'>
+              <RotatedArrowIcon key={`wind-icon-${hourlyForecast.forecast.id}`} rotation={mapWindDirection(hourlyForecast)} />
             </Tooltip>
-            <Text fontSize='xs'>{hourlyForecast.forecast.gustSpeedKph}</Text>
+            <Text key={`wind-label-${hourlyForecast.forecast.id}`} fontSize='xs'>{hourlyForecast.forecast.gustSpeedKph}</Text>
           </HStack>
         </Box>
       </Stack>
       <Divider orientation='vertical' key={`divider-${hourlyForecast.forecast.id}`}></Divider>
-    </>
+    </Box>
   )
 }
 
@@ -138,8 +198,6 @@ const ForecastRender = ({ locationForecasts }) => {
 
   const groupedAllDaysData = convertData(data);
   const firstDayData = groupedAllDaysData[0];
-
-  console.log(firstDayData.data[0]);
 
   return (
     <>
